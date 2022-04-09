a1734e1d84dfacd3c06f1278c71224e2c0e52815
tnoonan <tnoonan88@gmail.com>
2022-04-09 11:15:39 -0400

    debugged routes

--- models/Employer.js
+++ models/Employer.js
@@ -3,9 +3,9 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 class Employer extends Model {
-  checkPassword(loginPw) {
-    return bcrypt.compareSync(loginPw, this.password);
-  }
+  // checkPassword(loginPw) {
+  //   return bcrypt.compareSync(loginPw, this.password);
+  // }
 };
 
 Employer.init(
@@ -37,16 +37,16 @@ Employer.init(
     },
   },
   {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-        return updatedUserData;
-      },
-    },
+    // hooks: {
+    //   beforeCreate: async (newUserData) => {
+    //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
+    //     return newUserData;
+    //   },
+    //   beforeUpdate: async (updatedUserData) => {
+    //     updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+    //     return updatedUserData;
+    //   },
+    // },
     sequelize,
     timestamps: false,
     freezeTableName: true,
