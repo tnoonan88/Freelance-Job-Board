1b826ef128a75710eb01ef296fe56f3bf0e45bd8
tnoonan <tnoonan88@gmail.com>
2022-04-09 12:51:27 -0400

    updated associations

--- models/index.js
+++ models/index.js
@@ -3,5 +3,14 @@ const Job = require('./Job');
 const User = require('./User');
 
 // Write associations
+Job.belongsTo(Employer, {
+    foreignKey: 'employer_id',
+    onDelete: 'CASCADE'
+});
+
+Employer.hasMany(Job, {
+    foreignKey: 'employer_id',
+});
+
 
 module.exports = { Employer, Job, User };
