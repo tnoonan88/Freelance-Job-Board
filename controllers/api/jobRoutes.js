2ad11f3ec87f8f72610e623dedfe8f8ec5548317
Zach P <95437073+picks6@users.noreply.github.com>
2022-04-13 19:27:55 -0400

    updated job route

--- controllers/api/jobRoutes.js
+++ controllers/api/jobRoutes.js
@@ -16,7 +16,27 @@ router.get('/', (req, res) => {
 
 router.get('/search/:posting', (req, res) => {
   // Job.findByPk
-})
+  Job.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((results) => {
+      if (!results) {
+        res.status(404).json({
+          message: `No match for ${req.params.id}. Please try again with different ID.`,
+        });
+        return;
+      }
+      
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 
 //Add route
 
