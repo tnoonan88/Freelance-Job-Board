875d2ae172faa8fceeb783843d4e2ac2dc94362a
Zach P <95437073+picks6@users.noreply.github.com>
2022-04-15 17:33:18 -0400

    fixed login redirect

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
 
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/");
+    res.redirect("/listings");
     return;
   }
   res.render("login");
